aefcd52c4cd4f29cd5b636b93dde2908a3260d04
Syafiq Basri <altsyafiq@gmail.com>
2023-04-26 12:07:09 +0800

    refactor: reformatted using black and updated function names

--- api/scripts/scrapper.py
+++ api/scripts/scrapper.py
@@ -10,11 +10,12 @@ from sqlalchemy import Date
 
 BASE_URL = "https://www.e-solat.gov.my/index.php"
 SCHEDULE_PARAM = "?siteId=24&pageId=24"
-SQLITE_PATH = "sql_app.db" # SQLITE path in virtualenv
+SQLITE_PATH = "sql_app.db"  # SQLITE path in virtualenv
 
 
 class Period(Enum):
-    """ Period option for API """
+    """Period option for API"""
+
     WEEK = "week"
     MONTH = "month"
     YEAR = "year"
@@ -30,7 +31,7 @@ class PrayerTimeScrapper:
         self.scrapped = []
 
     def get_zones(self):
-        """ Scrapping zones from site search dropdown options """
+        """Scrapping zones from site search dropdown options"""
         print("\033[94mINFO\033[0m Scrapping zones..")
         zone_res = self.root_soup.find(id="inputZone")
         zone_labels = zone_res.select("optgroup[label]")
@@ -46,7 +47,7 @@ class PrayerTimeScrapper:
         return self.zones
 
     def get_prayer_time(self, period=Period.MONTH.value):
-        """ Scrapping prayer times from site API """
+        """Scrapping prayer times from site API"""
         print("\033[94mINFO\033[0m Scrapping prayer times..")
         # https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period={period}&zone={zone}
         base_req_url = f"{BASE_URL}?r=esolatApi/takwimsolat&period={period}&zone="
@@ -57,9 +58,14 @@ class PrayerTimeScrapper:
             cur_res = requests.get(cur_req_url)
             cur_res_json = cur_res.json()
 
-            if cur_res.status_code == requests.codes.ok and cur_res_json["status"] == "OK!":
+            if (
+                cur_res.status_code == requests.codes.ok
+                and cur_res_json["status"] == "OK!"
+            ):
                 code_key = cur_res_json["zone"]
-                self.scrapped = self.scrapped + [{ f"{code_key}": cur_res_json["prayerTime"] }]
+                self.scrapped = self.scrapped + [
+                    {f"{code_key}": cur_res_json["prayerTime"]}
+                ]
 
     def init_db(self):
         conn = None
@@ -69,7 +75,7 @@ class PrayerTimeScrapper:
             print(e)
 
         return conn
-    
+
     def sync_zones(self):
         """
         Sync zones to database, will add new data is code
@@ -77,8 +83,9 @@ class PrayerTimeScrapper:
         will update the data in database
         """
         print("\033[94mINFO\033[0m Syncing zones..")
+
         def get_db_zones():
-            """ Fetch zones from database to compare """
+            """Fetch zones from database to compare"""
             select_sql = """
                 SELECT *
                 FROM zones;
@@ -87,9 +94,9 @@ class PrayerTimeScrapper:
             curr.execute(select_sql)
 
             return curr.fetchall()
-        
-        def add_new_entry(new_code, new_text, new_state):
-            """ Insert new entry to database """
+
+        def add_new_zone(new_code, new_text, new_state):
+            """Insert new entry to database"""
             insert_sql = """
                 INSERT INTO zones (code, text, state, created_at)
                 VALUES (?, ?, ?, ?)
@@ -97,9 +104,9 @@ class PrayerTimeScrapper:
             insert_val = (new_code, new_text, new_state, datetime.datetime.utcnow())
             curr = conn.cursor()
             curr.execute(insert_sql, insert_val)
-        
-        def patch_entry(new_code, new_text, new_state, id):
-            """ Patch entry in database """
+
+        def patch_zone(new_code, new_text, new_state, id):
+            """Patch entry in database"""
             insert_sql = """
                     UPDATE zones
                     SET code = ?,
@@ -111,21 +118,25 @@ class PrayerTimeScrapper:
             insert_val = (new_code, new_text, new_state, datetime.datetime.utcnow(), id)
             curr = conn.cursor()
             curr.execute(insert_sql, insert_val)
-            
+
         if self.zones:
             conn = self.init_db()
             zones_in_db = get_db_zones()
 
             for zo in self.zones:
-                from_db = [db_zone for db_zone in zones_in_db if db_zone[1] == zo["code"]]
+                from_db = [
+                    db_zone for db_zone in zones_in_db if db_zone[1] == zo["code"]
+                ]
                 is_no_data = not from_db
-                is_need_update = is_no_data or (zo["code"] != from_db[0][1] or
-                                                zo["text"] != from_db[0][2] or
-                                                zo["state"] != from_db[0][3])
+                is_need_update = is_no_data or (
+                    zo["code"] != from_db[0][1]
+                    or zo["text"] != from_db[0][2]
+                    or zo["state"] != from_db[0][3]
+                )
                 if not from_db:
-                    add_new_entry(zo["code"], zo["text"], zo["state"])
+                    add_new_zone(zo["code"], zo["text"], zo["state"])
                 elif from_db and is_need_update:
-                    patch_entry(zo["code"], zo["text"], zo["state"], from_db[0][0])
+                    patch_zone(zo["code"], zo["text"], zo["state"], from_db[0][0])
 
             conn.commit()
             self.zones_in_db = get_db_zones()
@@ -140,6 +151,7 @@ class PrayerTimeScrapper:
         TODO: make it simpler
         """
         print("\033[94mINFO\033[0m Syncing prayer times..")
+
         def get_db_prayer_times():
             select_sql = """
                 SELECT *
@@ -152,22 +164,24 @@ class PrayerTimeScrapper:
             curr.execute(select_sql, select_value)
 
             return curr.fetchall()
-        
+
         def add_new_prayer_time(**kwargs):
             insert_sql = """
                 INSERT INTO prayer_times (zone_id, hijri_date, date, imsak, fajr, syuruk, dhuhr, asr, maghrib, isha)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """ 
-            insert_val = (kwargs.get("zone_id"),
-                          kwargs.get("hijri"),
-                          kwargs.get("date"),
-                          kwargs.get("imsak"),
-                          kwargs.get("fajr"),
-                          kwargs.get("syuruk"),
-                          kwargs.get("dhuhr"),
-                          kwargs.get("asr"),
-                          kwargs.get("maghrib"),
-                          kwargs.get("isha"),)
+            """
+            insert_val = (
+                kwargs.get("zone_id"),
+                kwargs.get("hijri"),
+                kwargs.get("date"),
+                kwargs.get("imsak"),
+                kwargs.get("fajr"),
+                kwargs.get("syuruk"),
+                kwargs.get("dhuhr"),
+                kwargs.get("asr"),
+                kwargs.get("maghrib"),
+                kwargs.get("isha"),
+            )
             curr = conn.cursor()
             curr.execute(insert_sql, insert_val)
             print("\033[96mADDED\033[0m Adding", kwargs.get("zone"), kwargs.get("date"))
@@ -178,33 +192,45 @@ class PrayerTimeScrapper:
         if not current_month_data_db:
             for by_zone_code in self.scrapped:
                 current_zone = list(by_zone_code.keys())[0]
-                zone_id = [zdb[0] for zdb in self.zones_in_db if zdb[1] == current_zone][0]
+                zone_id = [
+                    zdb[0] for zdb in self.zones_in_db if zdb[1] == current_zone
+                ][0]
 
                 if not zone_id:
                     continue
-                
+
                 for daily in by_zone_code[current_zone]:
-                    converted_date = datetime.datetime.strptime(daily['date'], '%d-%b-%Y').strftime("%Y-%m-%d")
-                    add_new_prayer_time(zone=current_zone,
-                                        zone_id=zone_id,
-                                        hijri=daily["hijri"],
-                                        date=converted_date,
-                                        imsak=daily["imsak"],
-                                        fajr=daily["fajr"],
-                                        syuruk=daily["syuruk"],
-                                        dhuhr=daily["dhuhr"],
-                                        asr=daily["asr"],
-                                        maghrib=daily["maghrib"],
-                                        isha=daily["isha"])
-            
+                    converted_date = datetime.datetime.strptime(
+                        daily["date"], "%d-%b-%Y"
+                    ).strftime("%Y-%m-%d")
+                    add_new_prayer_time(
+                        zone=current_zone,
+                        zone_id=zone_id,
+                        hijri=daily["hijri"],
+                        date=converted_date,
+                        imsak=daily["imsak"],
+                        fajr=daily["fajr"],
+                        syuruk=daily["syuruk"],
+                        dhuhr=daily["dhuhr"],
+                        asr=daily["asr"],
+                        maghrib=daily["maghrib"],
+                        isha=daily["isha"],
+                    )
+
             print("\033[92mDONE\033[0m Completed")
         else:
-            print('\033[93mERROR\033[0m!! Data not synced, current month data is already in database')
+            print(
+                "\033[93mERROR\033[0m!! Data not synced, current month data is already in database"
+            )
 
         conn.commit()
         conn.close()
 
+
 if __name__ == "__main__":
+    """
+    TODO: add argument parser
+    """
     scrapper = PrayerTimeScrapper()
 
     # Stage 1: get zones
