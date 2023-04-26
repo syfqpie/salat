33195b039a4d2b95508415c81841e4bed93094aa
Syafiq Basri <altsyafiq@gmail.com>
2023-04-26 21:27:43 +0800

    docs: added docstring

--- api/app/operations/prayer_time/crud.py
+++ api/app/operations/prayer_time/crud.py
@@ -7,10 +7,12 @@ from app.operations.prayer_time import models
 
 
 def get_zones(db: Session):
+    """Query all zones"""
     return db.query(models.Zone).all()
 
 
 def get_zones_by_state(db: Session, state: str):
+    """Query zones by state"""
     return db.query(models.Zone).filter(models.Zone.state == state).all()
 
 
