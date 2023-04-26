5f9a3d407eb4ef5d475f1dc8991c9f87a0cca4d1
Syafiq Basri <altsyafiq@gmail.com>
2023-04-26 12:49:11 +0800

    feat: added core files

---
+++ api/app/core/settings.py
@@ -0,0 +1,40 @@
+import ast
+from decouple import config
+
+from typing import Any, List, Union
+from pydantic import AnyHttpUrl, BaseSettings, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = config("PROJECT_NAME", "")
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = config("BACKEND_CORS_ORIGINS", [])
+
+    class Config:
+        case_sensitive = True
+
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            """
+            Override default function to add custom scenario
+            """
+            if field_name == "BACKEND_CORS_ORIGINS":
+                return cls._assemble_cors_origins(raw_val)
+
+            return cls.json_loads(raw_val)
+
+        def _assemble_cors_origins(raw_val: Union[str, List[str]]) -> List[AnyHttpUrl]:
+            """
+            Process BACKEND_CORS_ORIGINS value from env and convert into a list
+            """
+            if isinstance(raw_val, str) and not raw_val.startswith("["):
+                return [i.strip() for i in raw_val.split(",")]
+            elif isinstance(raw_val, (list, str)):
+                # Return converted str representation of list to list
+                return ast.literal_eval(raw_val)
+            elif isinstance(raw_val, list):
+                # If raw_val already a list, return self
+                return raw_val
+
+
+settings = Settings()
