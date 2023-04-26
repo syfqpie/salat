15e3f1f86735eed7d001b7c7d216a51cdd1e408f
Syafiq Basri <altsyafiq@gmail.com>
2023-04-27 02:23:14 +0800

    feat: added main file

---
+++ api/app/main.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.v1.api import api_router
+from app.core.settings import settings
+from app.core.database import Base, engine
+
+
+@lru_cache()
+def get_settings():
+    # https://fastapi.tiangolo.com/advanced/settings/#creating-the-settings-only-once-with-lru_cache
+    return settings
+
+
+app = FastAPI(
+    title=get_settings().PROJECT_NAME,
+    openapi_url=f"{ get_settings().API_V1_STR }/openapi.json",
+)
+
+# Database
+Base.metadata.create_all(bind=engine)
+
+# CORS config
+# https://fastapi.tiangolo.com/tutorial/cors/#use-corsmiddleware
+if get_settings().BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in get_settings().BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+# Router
+app.include_router(api_router, prefix=get_settings().API_V1_STR)
