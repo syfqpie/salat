8b4586fbb9294c042eacb50ab82eefc12a670c84
Syafiq Basri <altsyafiq@gmail.com>
2023-04-29 13:09:07 +0800

    refactor: changed to use computed values

--- web/components/PrayerDaily.vue
+++ web/components/PrayerDaily.vue
@@ -7,23 +7,20 @@ div
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import { getters } from '~/store/prayerTimes'
 import { PrayerTime } from '~/types/prayer-times'
 
 export default defineComponent({
 	name: 'PrayerDaily',
-	data () {
-		return {
-			todayPrayerTimes: null as PrayerTime | null,
-			tomorrowPrayerTimes: null as PrayerTime | null
+	computed: {
+		todayPrayerTimes (): PrayerTime | null {
+			return this.$store.getters['prayerTimes/today']
+		},
+		tomorrowPrayerTimes (): PrayerTime | null {
+			return this.$store.getters['prayerTimes/tomorrow']
 		}
 	},
 	mounted () {
 		this.$store.dispatch('prayerTimes/getData')
-			.then(() => {
-				this.todayPrayerTimes = this.$store.getters['prayerTimes/today'] as ReturnType<typeof getters['prayerTimes/today']>
-				this.tomorrowPrayerTimes = this.$store.getters['prayerTimes/tomorrow'] as ReturnType<typeof getters['prayerTimes/tomorrow']>
-			})
 	}
 })
 </script>
